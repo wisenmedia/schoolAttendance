ac25c4762ed1f3fdfa501b7d0aee834c00279359
wisen.media <wisenmedia@gmail.com>
2026-05-05 00:49:34 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// import packages
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import ejs from "ejs";
+
+//create express app
+const app = express();
+//port number
+const port = 3000;
+
+//create app middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//create database connection
+const pool = new pg.Pool({
+  user: 'postgres',
+  host: 'localhost',  
+    database: 'school',
+    password: 'Benson6969$',
+    port: 5432, // default PostgreSQL port
+});
+//Open Database bd connection 
+pool.connect();
+
+//create listener
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
